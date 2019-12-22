9386be6e85721958029f1f3e4b6b42d44164ee62
Rameş Aliyev <creator@ramesaliyev.com>
2019-12-23 00:25:41 +0300

    Update description & remote pdf rendering

--- src/render.js
+++ src/render.js
@@ -2,22 +2,9 @@ const fs = require('fs');
 const path = require("path");
 const pug = require('pug');
 const data = require('./data.json');
-const pdf = require('html-pdf');
 
 // Compile the source code
 const compiledFunction = pug.compileFile(__dirname + '/cv.pug');
 const html = compiledFunction(data);
 
 fs.writeFileSync(path.join(__dirname + '/../index.html'), html);
-
-pdf
-  .create(html, {})
-  .toFile(path.join(__dirname + '/../cv.pdf'),
-    (err, res) => {
-      if (err) {
-        return console.log(err);
-      }
-
-    console.log(res);
-  }
-);
